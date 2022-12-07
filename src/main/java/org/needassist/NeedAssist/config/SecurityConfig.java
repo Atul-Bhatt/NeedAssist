200bd0ac2ddd186c57da43df0fd56aeb306b4708
Atul Bhatt <atulbhatt61@gmail.com>
2022-12-07 21:23:59 +0530

    Added User Model and Authentication.

---
+++ src/main/java/org/needassist/NeedAssist/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.needassist.NeedAssist.config;
+
+import org.needassist.NeedAssist.service.JpaUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JpaUserDetailsService jpaUserDetailsService;
+
+    public SecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
+        this.jpaUserDetailsService = jpaUserDetailsService;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .userDetailsService(jpaUserDetailsService)
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .build();
+    }
+}
--- src/main/java/org/needassist/NeedAssist/controller/HomeController.java
+++ src/main/java/org/needassist/NeedAssist/controller/HomeController.java
@@ -1,5 +1,8 @@
 package org.needassist.NeedAssist.controller;
 
+import org.needassist.NeedAssist.model.User;
+import org.needassist.NeedAssist.repository.UserRepository;
+import org.needassist.NeedAssist.service.JpaUserDetailsService;
 import org.needassist.NeedAssist.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
     PostService postService;
 
     @GetMapping("/")
     public String home(Model model) {
+        userRepository.save(new User("user1", "password1", "user_role"));
+        userRepository.save(new User("user2", "password2", "admin_role"));
         model.addAttribute("allPosts", postService.getAllPosts());
         return "home";
     }
