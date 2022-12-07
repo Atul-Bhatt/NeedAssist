1d83df289dc0bb23fc6e9e9f2efe6a326dc9ca74
Atul Bhatt <atulbhatt61@gmail.com>
2022-12-07 09:02:33 +0530

    Need Assist

---
+++ src/test/java/org/needassist/NeedAssist/NeedAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.needassist.NeedAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
