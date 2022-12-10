da153d010b6a1b077f016b8988c94fc28af9a896
Atul Bhatt <atulbhatt61@gmail.com>
2022-12-10 22:34:34 +0530

    Edit post working, redirecting edit, created posts to home.

--- src/main/java/org/needassist/NeedAssist/controller/HomeController.java
+++ src/main/java/org/needassist/NeedAssist/controller/HomeController.java
@@ -8,8 +8,10 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -31,10 +33,18 @@ public class HomeController {
     }
 
     @PostMapping("/submit")
-    public void postSubmit(@ModelAttribute Post post, Model model) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        model.addAttribute("post", post);
+    public ModelAndView postSubmit(@ModelAttribute Post post) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         post.setUserId(postService.getUserIdFromContext());
         postRepository.save(post);
+        return new ModelAndView("redirect:/");
+    }
+
+    @PostMapping("/update/{postId}")
+    public ModelAndView postUpdate(@ModelAttribute Post post, @PathVariable("postId") int postId) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        post.setUserId(postService.getUserIdFromContext());
+        post.setId(postId);
+        postRepository.save(post);
+        return new ModelAndView("redirect:/");
     }
 
     @GetMapping("/myposts")
@@ -44,13 +54,21 @@ public class HomeController {
     }
 
     @GetMapping("/delete/{postId}")
-    public String deletePost(@PathVariable("postId") int postId) {
+    public ModelAndView deletePost(@PathVariable("postId") int postId) {
         try {
             postRepository.deleteById(postId);
         } catch(EmptyResultDataAccessException e) {
             System.out.println("Post Id does not exist!");
         }
-        return "home";
+        return new ModelAndView("redirect:/");
+    }
+
+    @GetMapping("/update/{postId}")
+    public String update(Model model, @PathVariable("postId") int postId) {
+        Optional<Post> post = postRepository.findById(postId);
+        model.addAttribute("post", post.orElse(new Post()));
+        model.addAttribute("postId", postId);
+        return "update";
     }
 
 }
